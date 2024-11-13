bdce3c512b647e2da1b3cc65e06530172300af77
onlymatrix <yyzyq2105@gmail.com>
2024-11-13 15:05:43 +0800

    Add MiniCPM-1B-sft-bf16

---
+++ MiniCPM-1B-sft-bf16/README.md
@@ -0,0 +1,20 @@
+# OpenVINO™ backend on MiniCPM-1B-sft-bf16
+* Model: https://huggingface.co/openbmb/MiniCPM-1B-sft-bf16
+* Step 1. create python environment
+
+``` sh
+conda create -n ov_minicpm python=3.10
+conda activate ov_minicpm
+pip install -r requirements.txt
+```
+
+* Step2. Convert minicpmV26 model to OpenVINO™ IR(Intermediate Representation). Be patient, it may takes some time.
+``` sh
+python convert_miniCPM1B.py -m /path/to/MiniCPM-1B-sft-bf16 -o /path/to/MiniCPM-1B-sft-bf16_ov
+```
+<img src="./images/1.png" width="50%"></img>
+* Step3. Testing
+``` sh
+python chat_miniCPM1B.py -m /path/to/MiniCPM-1B-sft-bf16_ov -d "GPU"
+```
+<img src="./images/2.png" width="50%"></img>
---
+++ MiniCPM-1B-sft-bf16/chat_miniCPM1B.py
@@ -0,0 +1,126 @@
+import argparse
+from typing import List, Tuple
+from threading import Thread
+import torch
+from optimum.intel.openvino import OVModelForCausalLM
+from transformers import (AutoTokenizer, AutoConfig,
+                          TextIteratorStreamer, StoppingCriteriaList, StoppingCriteria)
+
+
+class StopOnTokens(StoppingCriteria):
+    def __init__(self, token_ids):
+        self.token_ids = token_ids
+
+    def __call__(
+            self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
+    ) -> bool:
+        for stop_id in self.token_ids:
+            if input_ids[0][-1] == stop_id:
+                return True
+        return False
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-h',
+                        '--help',
+                        action='help',
+                        help='Show this help message and exit.')
+    parser.add_argument('-m',
+                        '--model_path',
+                        required=True,
+                        type=str,
+                        help='Required. model path')
+    parser.add_argument('-l',
+                        '--max_sequence_length',
+                        default=256,
+                        required=False,
+                        type=int,
+                        help='Required. maximun length of output')
+    parser.add_argument('-d',
+                        '--device',
+                        default='CPU',
+                        required=False,
+                        type=str,
+                        help='Required. device for inference')
+    args = parser.parse_args()
+    model_dir = args.model_path
+
+    ov_config = {"PERFORMANCE_HINT": "LATENCY",
+                 "NUM_STREAMS": "1", "CACHE_DIR": ""}
+
+    tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
+    print("====Compiling model====")
+    ov_model = OVModelForCausalLM.from_pretrained(
+        model_dir,
+        device=args.device,
+        ov_config=ov_config,
+        config=AutoConfig.from_pretrained(model_dir, trust_remote_code=True),
+        trust_remote_code=True
+    )
+
+    streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=True)
+    stop_tokens = [151643, 151645]
+    stop_tokens = [StopOnTokens(stop_tokens)]
+
+    def convert_history_to_token(history: List[Tuple[str, str]]):
+
+        messages = []
+        for idx, (user_msg, model_msg) in enumerate(history):
+            if idx == len(history) - 1 and not model_msg:
+                messages.append({"role": "user", "content": user_msg})
+                break
+            if user_msg:
+                messages.append({"role": "user", "content": user_msg})
+            if model_msg:
+                messages.append({"role": "assistant", "content": model_msg})
+
+        model_inputs = tokenizer.apply_chat_template(messages,
+                                                     add_generation_prompt=True,
+                                                     tokenize=True,
+                                                     return_tensors="pt")
+        return model_inputs
+
+    history = []
+    print("====Starting conversation====")
+    while True:
+        input_text = input("用户: ")
+        if input_text.lower() == 'stop':
+            break
+
+        if input_text.lower() == 'clear':
+            history = []
+            print("AI: 对话历史已清空")
+            continue
+
+        print("AI: ", end=" ")
+        history = history + [[input_text, ""]]
+        model_inputs = convert_history_to_token(history)
+        generate_kwargs = dict(
+            input_ids=model_inputs,
+            max_new_tokens=args.max_sequence_length,
+            temperature=0.1,
+            do_sample=True,
+            top_p=0.8,
+            top_k=100,
+            repetition_penalty=1.05,
+            streamer=streamer,
+            stopping_criteria=StoppingCriteriaList(stop_tokens),
+            pad_token_id=151645,
+        )
+
+        t1 = Thread(target=ov_model.generate, kwargs=generate_kwargs)
+        t1.start()
+
+        partial_text = ""
+        for new_text in streamer:
+            print(new_text, end="", flush=True)
+            partial_text += new_text
+        print("\n")
+        history[-1][1] = partial_text
+
+# python chat_miniCPM1B.py --model_path C:\work\modelbest\MiniCPM-1B-sft-bf16-ov-model --max_sequence_length 4096 --device CPU
+# 1.请介绍一些北京的景点。
+# 2.请介绍一下故宫。
+# 3.山东省最高的山是什么？
+# 4.请介绍英特尔公司。
